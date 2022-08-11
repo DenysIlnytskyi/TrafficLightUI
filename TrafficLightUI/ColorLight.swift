8e85546ccb136f23f1bcaac3351be53711ba850d
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-11 21:05:13 +0200

    Add new element(circle )

---
+++ TrafficLightUI/ColorLight.swift
@@ -0,0 +1,20 @@
+//
+//  ColorLight.swift
+//  TrafficLightUI
+//
+//  Created by Денис Ільницький on 11/08/2022.
+//
+
+import SwiftUI
+
+struct ColorLight: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorLight_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorLight()
+    }
+}

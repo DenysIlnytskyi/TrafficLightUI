5e0f985daa43c4660825aa0cbb65be975304c2ea
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-11 21:53:30 +0200

    Work with logic

--- TrafficLightUI/ContentView.swift
+++ TrafficLightUI/ContentView.swift
@@ -7,10 +7,65 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
+
 struct ContentView: View {
+    
+    @State private var buttonTitle = "START"
+    
+    @State private var redLightState = 0.3
+    @State private var yellowLightState = 0.3
+    @State private var greenLightState = 0.3
+    
+    @State private var currentLight = CurrentLight.red
+    
+    private func changeColor() {
+        
+        let lightIsOn = 1.0
+        let ligtIsOf = 0.3
+        
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redLightState = lightIsOn
+            greenLightState = ligtIsOf
+        case .yellow:
+            currentLight = .green
+            redLightState = ligtIsOf
+            yellowLightState = lightIsOn
+        case .green:
+            currentLight = .red
+            greenLightState = lightIsOn
+            yellowLightState = ligtIsOf
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20){
+                ColorLight(color: .red, opacity: redLightState)
+                ColorLight(color: .yellow, opacity: yellowLightState)
+                ColorLight(color: .green, opacity: greenLightState)
+                
+                Spacer()
+                
+                ChangeColorButton(tittle: buttonTitle) {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    changeColor()
+                }
+                
+            }
             .padding()
+        }
     }
 }
 
